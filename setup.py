a9afc13fde25a19cbed16c898dd9651953405cb3
barakbeilin <barak.beilin@gmail.com>
2020-04-14 07:54:57 +0300

    created package and upate setup py

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup
+from setuptools import setup
+from setuptools.command.develop import develop
+
+
+class OldDevelopCommand(develop):
+    def run(self):
+        print("magic_hw")
+        install.run(self)
+
+
+class CustomDevelopCommand(OldDevelopCommand):
+
+    def run(self):
+        super().run(super())
+        print("Hello, developer, how are you? :)")
+        install.run(self)
+
+
 ######
 setup(
     name="vegitelegram",
